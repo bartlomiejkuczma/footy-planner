8a4985085634f9943f02c3d80837d938fcaae268
Bartlomiej Kuczma <bartlomiejkuczma93@gmail.com>
2023-01-01 02:30:02 +0000

    feat(app): adding iconify icon framework

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let navItems = [
+		{ text: 'Fixture', href: '/', icon: 'mdi:calendar-cursor' },
+		{ text: 'Location', href: '/', icon: 'mdi:map-marker-multiple' },
+		{ text: 'How To Pay', href: '/', icon: 'bx:pound' },
+		{ text: 'Fixture Builder', href: '/', icon: 'game-icons:babyfoot-players' }
+	];
+</script>
+
+<nav>
+	<ul class="flex text-sm">
+		{#each navItems as navItem}
+			<li class="flex flex-col items-center py-2 flex-1 whitespace-nowrap">
+				<iconify-icon class="text-2xl" icon={navItem.icon} />
+				<a href={navItem.href}>{navItem.text}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import 'iconify-icon';
+	import Navbar from '$lib/Navbar.svelte';
 </script>
 
-<slot />
+<div class="container h-screen flex flex-col">
+	<div class="grow">
+		<slot />
+	</div>
+	<Navbar />
+</div>
