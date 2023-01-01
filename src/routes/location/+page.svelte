467f5dc39721e4552f660a20a5c606094cfa901a
Bartlomiej Kuczma <bartlomiejkuczma93@gmail.com>
2023-01-01 18:53:22 +0000

    feat(location): adding static content to location page

--- src/routes/location/+page.svelte
+++ src/routes/location/+page.svelte
@@ -5,4 +5,16 @@
 	headerSubTitle.set('Home of magic and beautiful football');
 </script>
 
-<h1 class="text-3xl font-bold">Location</h1>
+<div class="flex grow">
+	<div class="card bg-grass-green text-white mt-auto">
+		<figure><img src="images/cotham.png" alt="Cotham Pitch" /></figure>
+		<div class="card-body gap-0 p-4">
+			<h2 class="card-title justify-center">Cotham School</h2>
+			<div class="border-t my-2" />
+			<p class="font-light">Cotham Lawn Rd</p>
+			<p class="font-light">Cotham</p>
+			<p class="font-light">Bristol</p>
+			<p class="font-light">BS6 6DT</p>
+		</div>
+	</div>
+</div>
