4fb461b51adecde05c8fe97e6235f756b0052c79
Bartlomiej Kuczma <bartlomiejkuczma93@gmail.com>
2023-01-01 19:02:49 +0000

    feat(how-to-pay): setting up static content for how to pay page

--- src/routes/how-to-pay/+page.svelte
+++ src/routes/how-to-pay/+page.svelte
@@ -5,4 +5,14 @@
 	headerSubTitle.set('Payment methods available for a dude to pay for the pitch...');
 </script>
 
-<h1 class="text-3xl font-bold">How to Pay</h1>
+<h2 class="text-lg font-bold text-slate-600 mt-auto">Bank Transfer</h2>
+<div class="card bg-grass-green text-white">
+	<div class="card-body gap-0 p-4 text-center">
+		<h2 class="card-title justify-center">Tiago Grillo</h2>
+		<div class="border-t my-2" />
+		<p class="text-sm font-light">Account Number</p>
+		<p class="font-bold">XXXXXXXX</p>
+		<p class="text-sm font-light">Sort Code</p>
+		<p class="font-bold">XX-XX-XX</p>
+	</div>
+</div>
