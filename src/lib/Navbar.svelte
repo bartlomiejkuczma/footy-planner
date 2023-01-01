c75e434f3409b991de381ded6f935a373c12f3e7
Bartlomiej Kuczma <bartlomiejkuczma93@gmail.com>
2023-01-01 10:28:06 +0000

    feat(app): adding static menu

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	let navItems = [
-		{ text: 'Fixture', href: '/', icon: 'mdi:calendar-cursor' },
 		{ text: 'Location', href: '/', icon: 'mdi:map-marker-multiple' },
 		{ text: 'How To Pay', href: '/', icon: 'bx:pound' },
-		{ text: 'Fixture Builder', href: '/', icon: 'game-icons:babyfoot-players' }
+		{ text: 'Next Game', href: '/', icon: 'mdi:calendar-cursor' },
+		{ text: 'Fixture Builder', href: '/', icon: 'game-icons:babyfoot-players' },
+		{ text: 'More', href: '/', icon: 'mdi:dots-vertical' }
 	];
 </script>
 
 <nav>
-	<ul class="flex text-sm">
+	<ul class="flex justify-evenly bg-slate-50 text-slate-600">
 		{#each navItems as navItem}
-			<li class="flex flex-col items-center py-2 flex-1 whitespace-nowrap">
+			<li class="flex flex-col items-center py-3 whitespace-nowrap">
 				<iconify-icon class="text-2xl" icon={navItem.icon} />
-				<a href={navItem.href}>{navItem.text}</a>
+				<a class="text-xs" href={navItem.href}>{navItem.text}</a>
 			</li>
 		{/each}
 	</ul>
