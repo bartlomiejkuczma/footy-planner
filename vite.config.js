d19de29cc6599e54c7c97dc17000c03f2a856e43
Bartlomiej Kuczma <bartlomiejkuczma93@gmail.com>
2023-01-02 01:03:04 +0000

    chore(imports): creating import aliases to remove long relative paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	resolve: {
+		alias: {
+			'@lib': path.resolve('./src/lib'),
+			'@lib/*': path.resolve('./src/lib/*'),
+			'@stores': path.resolve('./src/stores'),
+			'@stores/*': path.resolve('./src/stores/*')
+		}
 	}
 };
 
